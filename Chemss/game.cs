157d8ed008bfd568a725d4b8751ccfa12fdf7e6b
Bill Tansey <billtansey@pm.me>
2023-02-02 20:23:16 +0000

    added //

--- Chemss/game.cs
+++ Chemss/game.cs
@@ -43,6 +43,7 @@ public class game
             getusermove();
             //
             //
+            //
         }
         
         Console.WriteLine("Moving piece: " + frompeice + " to which square?\n");
