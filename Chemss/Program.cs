d33b459b8c284e52799c7839b87d40660270a053
Bill Tansey <billtansey@pm.me>
2023-02-02 19:48:21 +0000

    added stuff

--- Chemss/Program.cs
+++ Chemss/Program.cs
@@ -6,98 +6,63 @@ namespace Chemss
     {
         public static string[,] board = new string[8,8];
         
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            outputboard();
-            Console.WriteLine("Welcome to Chemss! A lightweight, console version of Chess. This is a work in progress, so please be patient! \nPlease type 'help' for a list of commands. \n");
+            clboard.initBoard();
+            clboard.outputboard();
+            Console.WriteLine("\nWelcome to Chemss! A lightweight, console version of Chess. This is a work in progress, so please be patient! \nPlease type 'help' for a list of commands. \n");
             string input = Console.ReadLine();
             getCommand(input);
         }
         
-        static void getCommand(string input)
+        public static void getCommand(string input)
         {
             string usrinput;
             switch (input)
             {
                 case "help":
+                    reset();
                     Console.WriteLine("\nhelp - Displays this message. \nelo - Sets the ELO (Chess skill rating) of the CPU opponent. \nplay - Starts a game of chess. \nhow - Displays the rules of chess. \nexit - Exits the program. \n");
                     usrinput = Console.ReadLine();
                     getCommand(usrinput);
                     break;
                 case "elo":
+                    reset();
                     //
                     break;
                 case "play":
+                    reset();
+                    game.getusermove();
                     //
                     break;
                 case "how":
+                    reset();
+                    Console.WriteLine("Chess is a two-player strategy game played on a checkered board. The objective is to checkmate the opponent's king, \nwhich means the king is in a position to be captured (in check) and there is no way to move the king \nout of capture (mate).");
+                    Console.WriteLine("\nEach player starts with 16 pieces: one king, one queen, two rooks, two knights, two bishops, and eight pawns. The \npieces move in specific ways: the king moves one square in any direction, the queen can move in any \ndirection any number of squares, the rook can move horizontally or vertically any number of squares, the \nbishop can move diagonally any number of squares, the knight moves in an L-shape, and the \npawns move forward one square but capture diagonally.");
+                    usrinput = Console.ReadLine();
+                    getCommand(usrinput);
                     //
                     break;
                 case "exit":
+                    reset();
                     Environment.Exit(0);
                     break;
+                case "random":
+                    reset();
+                    break;
                 default:
-                    Console.WriteLine("Invalid command. Please type 'help' for a list of commands. What a sausage you are.\n");
+                    reset();
+                    Console.WriteLine("Invalid Command: '" + input + "'. Please type 'help' for a list of commands. \n");
                     usrinput = Console.ReadLine();
                     getCommand(usrinput);
                     break;
             }
         }
-        
-        static void loadBoard()
+
+        public static void reset()
         {
-            //load the board with the pieces
-            board[0,0] = "1R";
-            board[0,1] = "1N";
-            board[0,2] = "1B";
-            board[0,3] = "1Q";
-            board[0,4] = "1K";
-            board[0,5] = "1B";
-            board[0,6] = "1N";
-            board[0,7] = "1R";
-            board[1,0] = "1P";
-            board[1,1] = "1P";
-            board[1,2] = "1P";
-            board[1,3] = "1P";
-            board[1,4] = "1P";
-            board[1,5] = "1P";
-            board[1,6] = "1P";
-            board[1,7] = "1P";
-            
-            board[6,0] = "2R";
-            board[6,1] = "2N";
-            board[6,2] = "2B";
-            board[6,3] = "2Q";
-            board[6,4] = "2K";
-            board[6,5] = "2B";
-            board[6,6] = "2N";
-            board[6,7] = "2R";
-            board[6,0] = "2P";
-            board[6,1] = "2P";
-            board[6,2] = "2P";
-            board[6,3] = "2P";
-            board[6,4] = "2P";
-            board[6,5] = "2P";
-            board[6,6] = "2P";
-            board[6,7] = "2P";
-        }
-
-        static void outputboard()
-        {
-            string Boardverytop = " _________________________________________________";
-            string Boardtop =   " |_____|_____|_____|_____|_____|_____|_____|_____|";
-            string Boardline8 = "8|     |     |     |     |     |     |     |     |";
-            string Boardline7 = "7|     |     |     |     |     |     |     |     |";
-            string Boardline6 = "6|     |     |     |     |     |     |     |     |";
-            string Boardline5 = "5|     |     |     |     |     |     |     |     |";
-            string Boardline4 = "4|     |     |     |     |     |     |     |     |";
-            string Boardline3 = "3|     |     |     |     |     |     |     |     |";
-            string Boardline2 = "2|     |     |     |     |     |     |     |     |";
-            string Boardline1 = "1|     |     |     |     |     |     |     |     |";
-            String Boardbottom = "    a     b     c     d     e     f     g     h";
-
-
-            Console.WriteLine(Boardverytop + "\n" + Boardline8 + "\n" + Boardtop + "\n" + Boardline7 + "\n" + Boardtop + "\n" + Boardline6 + "\n" + Boardtop + "\n" + Boardline5 + "\n" + Boardtop + "\n" + Boardline4 + "\n" + Boardtop + "\n" + Boardline3 + "\n" + Boardtop + "\n" + Boardline2 + "\n" + Boardtop + "\n" + Boardline1 + "\n" + Boardtop + "\n" + Boardbottom);
+            Console.Clear();
+            clboard.outputboard();
         }
     }
 }
---
+++ Chemss/game.cs
@@ -0,0 +1,59 @@
+﻿namespace Chemss;
+
+public class game
+{
+    public static void start()
+    {
+        clboard.outputboard();
+        Console.ReadLine();
+    }
+
+    public static void getusermove()
+    {
+        Program.reset();
+        Console.WriteLine("Which piece are you moving?. (eg. 'e2') \n");
+        string frominput = Console.ReadLine();
+
+        frominput = frominput.Replace('a', '0');
+        frominput = frominput.Replace('b', '1');
+        frominput = frominput.Replace('c', '2');
+        frominput = frominput.Replace('d', '3');
+        frominput = frominput.Replace('e', '4');
+        frominput = frominput.Replace('f', '5');
+        frominput = frominput.Replace('g', '6');
+        frominput = frominput.Replace('h', '7');
+        
+        int fromx = Convert.ToInt32(frominput[0]);
+        int fromy = Convert.ToInt32(frominput[1]);
+        
+
+        string frompeice = Program.board[fromx, fromy];
+        
+        Console.WriteLine(frompeice);
+        
+        
+        if (frompeice == " ")
+        {
+            Console.WriteLine("Invalid move. Please try again. \n");
+            getusermove();
+        }
+        else if (frompeice[0] == 'B')
+        {
+            Console.WriteLine("Invalid move. Please try again. \n");
+            getusermove();
+        }
+        
+        Console.WriteLine("Moving piece: " + frompeice + " to which square?\n");
+        string toinput = Console.ReadLine();
+        
+        toinput = toinput.Replace('a', '0');
+        toinput = toinput.Replace('b', '1');
+        toinput = toinput.Replace('c', '2');
+        toinput = toinput.Replace('d', '3');
+        toinput = toinput.Replace('e', '4');
+        toinput = toinput.Replace('f', '5');
+        toinput = toinput.Replace('g', '6');
+        toinput = toinput.Replace('h', '7');
+        
+    }
+}
